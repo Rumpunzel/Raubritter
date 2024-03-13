34cf1f1f4d4a03b8db6707b9fb285e6e67815347
Jamal Bencharki <jb@neckar.it>
2024-03-13 21:58:28 +0100

    some more stuff

--- gameplay/character.gd
+++ gameplay/character.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var speed := 128.0
 
+@onready var navgiation_agent: NavigationAgent2D = %NavigationAgent2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,8 +11,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	navgiation_agent.target_position = get_global_mouse_position()
 
 func _physics_process(delta: float) -> void:
-	var motion := get_global_mouse_position() - global_position
+	var motion := navgiation_agent.get_next_path_position()
+	print(motion)
 	move_and_collide(motion.normalized() * speed * delta)
