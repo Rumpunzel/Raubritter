56d94d677f7aa88ee6f35d45e4a639994bde94b6
Jamal Bencharki <jb@neckar.it>
2024-03-16 18:18:28 +0100

    implemented unit spawning

--- gameplay/unit_instance.gd
+++ gameplay/unit_instance.gd
@@ -19,21 +19,30 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		_character_controller.destination = _character_controller.get_global_mouse_position()
-	else:
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		_character_controller.destination = get_viewport().get_mouse_position()
+		return
+	if not destination:
+		var carts := get_tree().get_nodes_in_group("Cart")
+		for cart: CharacterController in carts:
+			var distance_to_cart := _character_controller.global_position.distance_squared_to(cart.global_position)
+			if not destination or distance_to_cart < _character_controller.global_position.distance_squared_to(destination.global_position):
+				destination = cart
 		if not destination: return
-		_character_controller.destination = destination.global_position
+	_character_controller.destination = destination.global_position
 
 func damage(damage_taken: float, _source: UnitInstance, _weapon: WeaponInstance) -> void:
 	hit_points -= damage_taken
-	print("took %f damage!" % damage_taken)
+	if hit_points <= 0.0: _die()
 
 func spawn(spawn_point: Node2D) -> void:
+	spawn_at(spawn_point.global_position)
+	_character_controller.rotation = spawn_point.rotation
+
+func spawn_at(spawn_position: Vector2) -> void:
 	_character_controller = unit.create_character_controller()
 	add_child(_character_controller)
-	_character_controller.global_position = spawn_point.global_position
-	_character_controller.rotation = spawn_point.rotation
+	_character_controller.global_position = spawn_position
 	_character_controller.connect_to_unit_instance(self)
 
 func get_position() -> Vector2:
@@ -43,6 +52,10 @@ func get_healthbar_position() -> Vector2:
 	if not _character_controller: return Vector2.ZERO
 	var in_world_position := _character_controller.global_position - Vector2(0.0, _character_controller.health_bar_offset)
 	return _character_controller.get_viewport_transform() * in_world_position
+
+func _die() -> void:
+	if _character_controller: _character_controller.queue_free()
+	queue_free()
 
 # Called from signal emitted by [WeaponInstance]
 func _on_attacked(weapon: Weapon, weapon_instance: WeaponInstance, hit_box: HitBox) -> void:
--- gameplay/units.gd
+++ gameplay/units.gd
@@ -5,9 +5,17 @@ extends Node
 @export var _hud: HUD
 
 @export var _cart: Unit
+@export var _thug: Unit
 
 func _ready() -> void:
 	_on_cart_spawn_timer_timeout()
+
+func _process(_delta: float) -> void:
+	if not Input.is_action_just_pressed("debug_spawn_thug"): return
+	var thug := _thug.create_unit_instance()
+	add_child(thug)
+	thug.spawn_at(get_viewport().get_mouse_position())
+	_hud.create_healthbar(thug)
 
 func _on_cart_spawn_timer_timeout() -> void:
 	var cart := _cart.create_unit_instance()
