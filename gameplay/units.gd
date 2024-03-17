4b2ba3e10f321301d678b89d7fc8209e34081150
Jamal Bencharki <jb@neckar.it>
2024-03-17 09:44:36 +0100

    simplified test level

--- gameplay/units.gd
+++ gameplay/units.gd
@@ -1,14 +1,14 @@
-class_name Units
+class_name UnitSpawner
 extends Node
 
-@export var _path_exits: PathExits
 @export var _hud: HUD
+@export var _path_exits: PathExits
 
-@export var _cart: Unit
+@export var _unit: Unit
 @export var _thug: Unit
 
 func _ready() -> void:
-	_on_cart_spawn_timer_timeout()
+	_on_spawn_timer_timeout()
 
 func _process(_delta: float) -> void:
 	if not Input.is_action_just_pressed("debug_spawn_thug"): return
@@ -17,10 +17,10 @@ func _process(_delta: float) -> void:
 	thug.spawn_at(get_viewport().get_mouse_position())
 	_hud.create_healthbar(thug)
 
-func _on_cart_spawn_timer_timeout() -> void:
-	var cart := _cart.create_unit_instance()
-	add_child(cart)
+func _on_spawn_timer_timeout() -> void:
+	var unit := _unit.create_unit_instance()
+	add_child(unit)
 	var spawn_point := _path_exits.get_random_exit()
-	cart.spawn(spawn_point)
-	cart.destination = _path_exits.get_random_destination(spawn_point)
-	_hud.create_healthbar(cart)
+	unit.spawn(spawn_point)
+	unit.destination = _path_exits.get_random_destination(spawn_point)
+	_hud.create_healthbar(unit)
