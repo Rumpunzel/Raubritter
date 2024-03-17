828580e66c3e528257135283bb758c250b7bbf1e
Jamal Bencharki <jb@neckar.it>
2024-03-17 13:56:43 +0100

    implemented coin collecting

--- level/player.gd
+++ level/player.gd
@@ -1,17 +1,18 @@
 extends Node
 
+signal gold_changed(gold: int)
+
 @export var _gold := 10 :
 	set(new_gold):
 		_gold = new_gold
-		_gui.update_gold(_gold)
+		gold_changed.emit(_gold)
 
 @export var _unit: Outlaw
 @export var _thug_spawner: UnitSpawner
-@export var _gui: GUI
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	gold_changed.emit(_gold)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -19,3 +20,9 @@ func _process(_delta: float) -> void:
 	if _unit.can_spawn_unit(_gold):
 		_gold -= _unit.cost
 		_thug_spawner.spawn_unit_at(get_viewport().get_mouse_position(), _unit)
+
+func _on_coin_spawned(coin: Coin) -> void:
+	coin.collected.connect(_on_coin_collected)
+
+func _on_coin_collected(coin_value: int) -> void:
+	_gold += coin_value
